8a5ad3ae97405bf17ba34428aaed04a031fd7faa
Gabe Cavazos <gabecavazos3@gmail.com>
2023-02-08 16:57:07 -0600

    control structures

---
+++ control_structures_exercise.py
@@ -0,0 +1,12 @@
+weekdays = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+
+input('what day of the week is it?')
+
+
+today = input('what day of the week is it?')
+if today == 'monday':
+        print('have a good week!')
+else:
+        print('it\'s not monday')
+        
+
